d429910ac7babf3c66ed1fae17052224f258139c
geekskai <geeks.kai@gmail.com>
2024-01-28 00:28:25 +0800

    Refactor page component and remove NavTools

---
+++ src/app/[locale]/FileUpload.tsx
@@ -0,0 +1,82 @@
+import Icons from "@/components/Icons";
+import { Button, Flex, Layout, Space, Typography, Upload, message } from "antd";
+import type { UploadProps } from "antd";
+import { useTranslations, useLocale } from "next-intl";
+
+const { Dragger } = Upload;
+
+const { Header, Sider, Content, Footer } = Layout;
+const { Text, Paragraph, Title } = Typography;
+
+const uploadProps: UploadProps = {
+  name: "file",
+  // multiple: true,
+  action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (status === "done") {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log("Dropped files", e.dataTransfer.files);
+  },
+};
+export default function FileUpload({ setHasFile }: { setHasFile: Function }) {
+  const t = useTranslations();
+  const locale = useLocale();
+
+  const handleUpload = (file: File) => {
+    const uploadFile = file;
+    setHasFile(true);
+  };
+  return (
+    <Flex vertical align="center" className="bg-[#E6F3FF] h-full">
+      <Layout className="max-w-7xl w-full items-center">
+        <Flex vertical align="center" className="w-full h-full bg-[#E6F3FF]">
+          <article className="py-[48px] text-center">
+            <h1 className="text-5xl font-black">Online Video Converter</h1>
+            <Text className="!text-gray-900 font-medium text-xl">
+              Use the free online video converter to change your video file
+              format.
+            </Text>
+          </article>
+
+          <Flex
+            justify="center"
+            className="bg-white rounded-2xl p-[24px] h-[360px] w-[1040px]"
+          >
+            <Dragger className="w-full" {...uploadProps}>
+              <Paragraph className="w-full">
+                <Button
+                  className="!flex m-auto"
+                  type="primary"
+                  icon={<Icons.Upload size="20px" />}
+                >
+                  Upload Files
+                </Button>
+              </Paragraph>
+              <Space direction="vertical" className="w-[278px]">
+                <h6 className="font-medium">
+                  Or drag and drop multiple video files here
+                </h6>
+                <Text className="!text-gray-400">
+                  Format supports mp4, avi, flv, mov, wmv Upload up to 3 files.
+                  Max file size: 500MB
+                </Text>
+              </Space>
+            </Dragger>
+          </Flex>
+        </Flex>
+      </Layout>
+      {/* <Footer className="max-w-7xl m-auto">
+  Media Modify ©2023 Created by Media Modify
+</Footer> */}
+    </Flex>
+  );
+}
--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import Icons from "./Icons";
 import LocaleSwitcher from "./LocaleSwitcher";
-import NavTools from "./NavTools";
+import Navbar from "./Navbar";
 import Logo from "./Logo";
 import { useTranslations } from "next-intl";
 import { Button, Flex } from "antd";
@@ -15,7 +15,7 @@ export function SiteHeader() {
       <div className="max-w-[1440px] m-auto flex md:px-[16px] lg:px-[22px] xl:px-[32px] md:py-[4px] lg:py-[6px] xl:py-[16px] h-[60px] items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex-center lg:gap-x-[10px] xl:gap-x-[48px]">
           <Logo />
-          <NavTools />
+          <Navbar />
         </div>
         <div className="flex items-center justify-end md:gap-x-[4px] lg:gap-x-[10px] xl:gap-x-[26px]">
           <Link
